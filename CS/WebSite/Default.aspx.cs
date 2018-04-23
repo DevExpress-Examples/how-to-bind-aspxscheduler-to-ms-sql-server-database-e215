d6050f9abbe15e5766c19e9ea16f6d29f87205d6
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebSite/Default.aspx.cs
@@ -0,0 +1,33 @@
+﻿#region #usings
+using DevExpress.Web.ASPxScheduler;
+using System;
+using System.Data.SqlClient;
+#endregion #usings
+
+public partial class _Default : System.Web.UI.Page {
+
+    protected void Page_Load(object sender, EventArgs e) {
+	}
+
+	protected void Page_Init(object sender, EventArgs e)
+	{
+	 	ASPxScheduler1.Start = new DateTime(2008, 07, 13);
+	}
+    #region #appointmentrowinserted
+    protected void ASPxScheduler1_AppointmentRowInserted(object sender, ASPxSchedulerDataInsertedEventArgs e) {
+        SqlConnection connection = new SqlConnection(CarsSchedulingDataSource.ConnectionString);
+        object id = null;
+        connection.Open();
+        try
+        {
+            using (SqlCommand cmd = new SqlCommand("SELECT IDENT_CURRENT('CarScheduling')", connection))
+                id = Convert.ToInt32(cmd.ExecuteScalar());
+            e.KeyFieldValue = id;
+        }
+        finally
+        {
+            connection.Close();
+        }
+    }
+    #endregion #appointmentrowinserted
+}
---
+++ VB/WebSite/Default.aspx.vb
@@ -0,0 +1,32 @@
+﻿Imports Microsoft.VisualBasic
+#Region "#usings"
+Imports DevExpress.Web.ASPxScheduler
+Imports System
+Imports System.Data.SqlClient
+#End Region ' #usings
+
+Partial Public Class _Default
+	Inherits System.Web.UI.Page
+
+	Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+	End Sub
+
+	Protected Sub Page_Init(ByVal sender As Object, ByVal e As EventArgs)
+		 ASPxScheduler1.Start = New DateTime(2008, 07, 13)
+	End Sub
+	#Region "#appointmentrowinserted"
+	Protected Sub ASPxScheduler1_AppointmentRowInserted(ByVal sender As Object, ByVal e As ASPxSchedulerDataInsertedEventArgs)
+		Dim connection As New SqlConnection(CarsSchedulingDataSource.ConnectionString)
+		Dim id As Object = Nothing
+		connection.Open()
+		Try
+			Using cmd As New SqlCommand("SELECT IDENT_CURRENT('CarScheduling')", connection)
+				id = Convert.ToInt32(cmd.ExecuteScalar())
+			End Using
+			e.KeyFieldValue = id
+		Finally
+			connection.Close()
+		End Try
+	End Sub
+	#End Region ' #appointmentrowinserted
+End Class
